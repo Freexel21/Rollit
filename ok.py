5dbe0cdf73c4d2d5f9a391d0b2a318e5c1b5eb35
Freexel21 <krishna.sharma@infinitircm.com>
2026-01-10 00:54:03 +0530

    OKOK

--- ok.py
+++ ok.py
@@ -1,8 +1,1 @@
-import random 
-while True:
-    roll = input("Roll the dice? y/n): ")
-    if roll.lower() == "y":
-        print("You rolled:", random.randint(1, 6))
-    else:
-        print("Goodbye!")
-        break
+print("Wassuuuuuuuuuuuuuuuuuuupppppppppppppppppppp")
